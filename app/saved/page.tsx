421120fa604e0e89156e548a0dfebe28563befd0
nexxdevv <cliff.sanchez.999@gmail.com>
2025-06-13 14:56:28 -0700

    fix saved items page

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -2,22 +2,21 @@
 "use client"
 
 import { useUserStore } from "@/stores/userStore"
-import { products } from "@/data/products"
 import ProductCard from "@/components/product-card/product-card-grid"
+import { Product } from "@/types/product"
 
-export default function LikesPage() {
-  const { favorites } = useUserStore()
-  const likedProducts = products.filter((p) => favorites.includes(p.id))
+export default function SavedItemsPage() {
+  const { savedProducts } = useUserStore()
 
   return (
     <div className="max-w-6xl mx-auto h-screen px-6 py-10">
-      {likedProducts.length === 0 ? (
+      {savedProducts.length === 0 ? (
         <p className="text-center text-gray-500 text-lg">
           You haven’t liked any products yet.
         </p>
       ) : (
         <div className="flex flex-col gap-4">
-          {likedProducts.map((product, index) => (
+          {savedProducts.map((product: Product, index: number) => (
             <ProductCard key={product.id} product={product} index={index} />
           ))}
         </div>
